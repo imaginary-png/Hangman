d70f4bf6ceeac1bce15e4d2c668544f89ecafd47
Will <70348218+imaginary-png@users.noreply.github.com>
2022-10-04 10:42:46 +1100

    added word list and functions

--- dictionary/lib/dictionary.ex
+++ dictionary/lib/dictionary.ex
@@ -13,6 +13,16 @@ defmodule Dictionary do
 
   """
   def hello do
+    IO.puts "Hello World"
     :world
+  end
+
+  def word_list do
+    words = File.read!("assets/words.txt")
+    String.split(words, ~r/\n/, trim: true)
+  end
+
+  def random_word do
+    Enum.random(word_list())
   end
 end
