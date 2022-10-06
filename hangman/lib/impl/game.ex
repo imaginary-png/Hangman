15fd82c5100ccc5383849f29fe52cec1a9ca068d
Will <70348218+imaginary-png@users.noreply.github.com>
2022-10-07 01:44:31 +1100

    refactored hangman  types to  Hangman.Type

--- hangman/lib/impl/game.ex
+++ hangman/lib/impl/game.ex
@@ -1,8 +1,10 @@
 defmodule Hangman.Impl.Game do
 
-  @type t :: %Hangman.Impl.Game{
+  alias Hangman.Type
+
+  @type t :: %__MODULE__{
     turns_left: integer(),
-    game_state: Hangman.State,
+    game_state: Type.state,
     letters: list(String.t),
     used: MapSet.t(String.t),
   }
@@ -11,14 +13,21 @@ defmodule Hangman.Impl.Game do
     turns_left: 7,
     game_state: :initializing,
     letters: [],
-    used: MapSet.new(),
+    used: MapSet.new()
   )
 
+  @spec new_game() :: t
   def new_game do
-        %Hangman.Impl.Game{
-          letters: Dictionary.random_word |> String.codepoints
-        }
+    new_game(Dictionary.random_word)
   end
+
+
+  @spec new_game(String.t) :: t
+  def new_game(word) do
+    %__MODULE__{
+      letters: word |> String.codepoints
+    }
+end
 
 
 end
