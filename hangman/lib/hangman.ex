e1b7d778cff42c1ada0b03575d52007e7511450e
Will <70348218+imaginary-png@users.noreply.github.com>
2022-11-01 08:55:25 +1100

    add dynamic supervisor to hangman

--- hangman/lib/hangman.ex
+++ hangman/lib/hangman.ex
@@ -2,17 +2,18 @@ defmodule Hangman do
 
   alias Hangman.Runtime.Server
   alias Hangman.Type
+  alias Hangman.Runtime.Application
 
   @opaque game  :: Server.t
   @type tally :: Type.tally
 
   @spec new_game() :: game
   def new_game do
-    {:ok, pid} = Server.start_link
+    {:ok, pid} = Application.start_game
     pid
   end
 
-  @spec make_move(game, String.t) :: {game, tally}
+  @spec make_move(game, String.t) :: tally
   def make_move(game, guess) do
     GenServer.call(game, {:make_move, guess})
   end
